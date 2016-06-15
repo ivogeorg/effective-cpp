c4ccedf38fce08f8b9c14e40d6de97c0c239c0e5
Ivo Georgiev <ivogeorg@gmail.com>
2016-06-15 16:53:26 -0700

    I01: variable type deduction.

--- i01_type_ded.cpp
+++ i01_type_ded.cpp
@@ -17,6 +17,9 @@ constexpr std::size_t arraySize(T (&)[N]) noexcept  // below on
     return N;                                         // and
 }
 
+// Undefined template to elicit error message from compiler
+template <typename T> class TypeDisplayer;
+
 int I01::operator()() {
     std::cout << "--- IO1 ---" << std::endl << std::endl;
 
@@ -30,6 +33,22 @@ int I01::operator()() {
     for (auto e: mappedVals)
         std::cout << i++ << ' ' << std::setw(20) << std::right << e << std::endl;
     std::cout << std::endl << std::endl;
+
+    std::cout << "Type deduction in the IDE" << std::endl << std::endl;
+
+    // (1) CLion 2016.1.3 doesn't yet have type deduction on hover or Alt+Enter
+    const int theAnswer = 42;
+
+    auto x = theAnswer;
+    auto y = &theAnswer;
+
+    // (2) To elicit type information from the compiler, uncomment the following two lines
+//    TypeDisplayer<decltype(x)> xType; // compiler error message reports x's type as int
+//    TypeDisplayer<decltype(y)> yType; // and y's type as const int *
+
+    // (3) Using RTTI
+    std::cout << typeid(x).name() << std::endl;    // display types for x and y
+    std::cout << typeid(y).name() << std::endl;    // i = int, PKi == int const *
 
     return mappedVals.size() == arraySize(keyVals);
 }
