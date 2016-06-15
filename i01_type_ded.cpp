7b877c5a982975abf7c9d59d34b0407b8e719c5a
Ivo Georgiev <ivogeorg@gmail.com>
2016-06-15 14:01:52 -0700

    I01: type deduction for arrays.

--- i01_type_ded.cpp
+++ i01_type_ded.cpp
@@ -2,3 +2,32 @@
 // Created by Ivo Georgiev on 6/15/16.
 //
 
+#include <cstddef>
+#include <array>
+#include <iostream>
+#include <iomanip>
+#include "Item.h"
+
+// The deduced type includes the array size!!!
+template<typename T, std::size_t N>                 // see info
+constexpr std::size_t arraySize(T (&)[N]) noexcept  // below on
+{                                                   // constexpr
+    return N;                                         // and
+}
+
+int I01::operator()() {
+    std::cout << "--- IO1 ---" << std::endl << std::endl;
+
+    std::cout << "Type deduction for arrays" << std::endl << std::endl;
+
+    int keyVals[] = { 1, 3, 7, 9, 11, 22, 35 };
+
+    std::array<int, arraySize(keyVals)> mappedVals; // type deduction here
+
+    int i = 0;
+    for (auto e: mappedVals)
+        std::cout << i++ << ' ' << std::setw(20) << std::right << e << std::endl;
+    std::cout << std::endl << std::endl;
+
+    return mappedVals.size() == arraySize(keyVals);
+}
--- main.cpp
+++ main.cpp
@@ -3,9 +3,21 @@
 //
 
 #include <iostream>
+#include <vector>
+#include "Item.h"
+
+int sandbox() {
+    std::vector<Item*> items;
+
+    items.push_back(new I01());
+
+    int ret = 0;
+    for (auto i: items)
+        ret += (*i)();
+}
 
 int main() {
 
+    return sandbox();
 
-    return 0;
 }
